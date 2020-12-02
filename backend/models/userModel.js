cd29989f3da2f3e690afd9577b427297c456a782
Spencer Lazzar <spencerlazzar@yahoo.com>
2020-12-01 21:44:40 -0800

    Up and Running

---
+++ backend/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Must have a name"],
+    unique: [true, "Double up! Already have that name"]
+  },
+  password: {
+    type: String,
+    required: [true, "Hey give me a password!"]
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+}, { timestamps: true })
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
